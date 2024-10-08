022b96ebae9183f65fcce0ff7b076022c34689a0
Kalle12345678 <kalle.ehrsson@elev.ga.lbs.se>
2024-10-08 11:41:16 +0200

    The game

---
+++ SpaceInvaders/SpaceInvaders/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour
+{
+    public Laser laserPrefab;
+    Laser laser;
+    float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            position.x += speed * Time.deltaTime;
+        }
+
+        Vector3 leftedge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightedge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        position.x = Mathf.Clamp(position.x, leftedge.x, rightedge.x);
+
+        transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space) && laser == null)
+        {
+            laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Missile") || collision.gameObject.layer == LayerMask.NameToLayer("Invader"))
+        {
+            GameManager.Instance.OnPlayerKilled(this);
+        }
+    }
+}
