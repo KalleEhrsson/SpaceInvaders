98770f9c45426aae562f3c08fda5f047db120e8d
Kalle12345678 <kalle.ehrsson@elev.ga.lbs.se>
2024-10-17 11:43:38 +0200

    Update ChangeScene.cs

--- SpaceInvaders/SpaceInvaders/Assets/Scripts/ChangeScene.cs
+++ SpaceInvaders/SpaceInvaders/Assets/Scripts/ChangeScene.cs
@@ -8,11 +8,13 @@ public class ChangeScene : MonoBehaviour
 {
   public void PlayGame()
     {
+        print("Play knappen funkar");
         SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
     {
+        print("Quit knappen funkar");
         Application.Quit();
     }
 }
