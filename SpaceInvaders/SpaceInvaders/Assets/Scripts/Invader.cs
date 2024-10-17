7e356eb27ce49eefba69d4199ffac384c92d0599
Kalle12345678 <kalle.ehrsson@elev.ga.lbs.se>
2024-10-17 20:22:28 +0200

    nåt

--- SpaceInvaders/SpaceInvaders/Assets/Scripts/Invader.cs
+++ SpaceInvaders/SpaceInvaders/Assets/Scripts/Invader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEditor.Build.Content;
 using UnityEngine;
 
@@ -12,28 +13,28 @@ public class Invader : MonoBehaviour
 {
     public Sprite[] animationSprites = new Sprite[2];
     public float animationTime;
+    public ParticleSystem deathEffectPrefab;
 
     SpriteRenderer spRend;
     int animationFrame;
-    // Start is called before the first frame update
-
     private void Awake()
     {
         spRend = GetComponent<SpriteRenderer>();
         spRend.sprite = animationSprites[0];
+
     }
 
     void Start()
     {
         //Anropar AnimateSprite med ett visst tidsintervall
-        InvokeRepeating( nameof(AnimateSprite) , animationTime, animationTime);
+        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
     }
 
-    //pandlar mellan olika sprited för att skapa en animation
+    //pandlar mellan olika sprited fï¿½r att skapa en animation
     private void AnimateSprite()
     {
         animationFrame++;
-        if(animationFrame >= animationSprites.Length)
+        if (animationFrame >= animationSprites.Length)
         {
             animationFrame = 0;
         }
@@ -42,13 +43,19 @@ public class Invader : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Laser"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Laser"))
         {
+            //Invadern dï¿½r
             GameManager.Instance.OnInvaderKilled(this);
             GameObject.Find("Main Camera").GetComponent<ScreenShakeCode>().ScreenFlash(0.1f);
             GameObject.Find("Main Camera").GetComponent<ScreenShakeCode>().ScreenShake(1);
+            
+            //Skapar, spelar och tar bort partiklar
+            ParticleSystem dieEffect = Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
+            dieEffect.Play();
+            Destroy(dieEffect.gameObject, dieEffect.main.duration);
         }
-        else if(collision.gameObject.layer == LayerMask.NameToLayer("Boundary")) //nått nedre kanten
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Boundary")) //nï¿½tt nedre kanten
         {
             GameManager.Instance.OnBoundaryReached();
         }
--- SpaceInvaders/SpaceInvaders/Assets/Scripts/Laser.cs
+++ SpaceInvaders/SpaceInvaders/Assets/Scripts/Laser.cs
@@ -7,9 +7,31 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Laser : Projectile
 {
+    public Sprite[] animationSprites = new Sprite[3];
+    public float animationTime;
+
+    SpriteRenderer spriteRenderer;
+    int animationFrame;
+
     private void Awake()
     {
         direction = Vector3.up;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = animationSprites[0];
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(animateSprite), animationTime, animationTime);
+    }
+
+    private void animateSprite()
+    {
+        animationFrame++;
+        if (animationFrame >= animationSprites.Length)
+        {
+            animationFrame = 0;
+        }
+        spriteRenderer.sprite = animationSprites[animationFrame];
     }
 
     void Update()
